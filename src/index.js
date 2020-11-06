e71963be7292e979b3ec4925c4c5e45d65ad5948
Eva-Sophie <eva.s.rueger@gmail.com>
2020-11-06 10:26:52 +0100

    Fixed date functions

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let month = months[now.getMonth()];
     let year = now.getFullYear();
-    let date = document.querySelector("#date");
+    let date = document.querySelector("#last-updated");
     date.innerHTML = `${dateMonth} ${month}, ${year}`;
 
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = days[now.getDay()];
     let hour = now.getHours();
-    if (hours < 10) {
-    hours = `0${hours}`;}
+    if (hour < 10) {
+    hour = `0${hours}`;}
     let minutes = now.getMinutes();
     if (minutes < 10) {
-    minutes = `0${minutes}`;}
-    let time = document.querySelector("#time");
-    time.innerHTML = `${day}, ${hour}:${minutes} PM`;
+    minutes = `0${minutes}`};
+    let moment = document.querySelector("#day");
+    moment.innerHTML = `${day}, ${hour}:${minutes} PM`;
 
     function showTemperature(response) {
       console.log(response.data);
