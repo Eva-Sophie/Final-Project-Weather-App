aa9ef443ae94d05c422efdc17f400d804752c25b
Eva-Sophie <eva.s.rueger@gmail.com>
2020-11-10 11:47:17 +0100

    Removed date

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
     let now = new Date();
-
-    let dateMonth = now.getDate();
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let month = months[now.getMonth()];
-    let year = now.getFullYear();
-    let date = document.querySelector("#date");
-    date.innerHTML = `${dateMonth} ${month}, ${year}`;
-
     function formatDate (timestamp){
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = days[now.getDay()];
